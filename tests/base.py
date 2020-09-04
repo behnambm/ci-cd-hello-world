b0fb5a66f0839cbacea2bf5daa88d89ca57a441b
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2020-09-04 19:54:02 +0430

    fix flake8 errors

--- tests/base.py
+++ tests/base.py
@@ -4,7 +4,8 @@ import unittest
 
 class MyTest(unittest.TestCase):
     def test_the_index_returning_value(self):
-        ''' this is just a simple unittest to see are tests work in circleci or not'''
+        ''' this is just a simple unittest
+        to see are tests work in circleci or not'''
         result = index()
         expected_value = 'hello, world with CI/CD'
         self.assertEqual(result, expected_value)
