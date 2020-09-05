cb1ed82c0caa43b72f7ce086e2cfb01047f41ca9
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2020-09-05 14:07:36 +0430

    test heroku env vars

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+import os
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'hello, world with CI/CD'
+    var = os.environ['TEST_ENV_VAR']
+    return 'hello, world with CI/CD ' + var
